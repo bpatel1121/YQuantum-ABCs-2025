8211b078c79055759547b1bea1043a03460f8e40
bpatel1121 <157432774+bpatel1121@users.noreply.github.com>
2025-04-13 04:00:34 -0700

    Update CatStateGens.py

--- Task1A/CatStateGens.py
+++ Task1A/CatStateGens.py
@@ -12,6 +12,9 @@ dq.plot.wigner(psi)
 plt.show()
 
 #3-Cat Sim
-psi = (dq.coherent(20, 2) + dq.coherent(20, -2) + dq.coherent(20, 2j)).unit()
+psi = (dq.coherent(20, 2) + 
+       dq.coherent(20, 2*np.exp(1j*2*np.pi/3)) + 
+       dq.coherent(20, 2*np.exp(1j*4*np.pi/3))
+      ).unit()
 dq.plot.wigner(psi)
 plt.show()

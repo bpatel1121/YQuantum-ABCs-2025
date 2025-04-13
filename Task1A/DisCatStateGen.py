65d7e06551fcf4135b27841354f08413a2d2b4b4
bpatel1121 <157432774+bpatel1121@users.noreply.github.com>
2025-04-12 21:53:31 -0700

    Add files via upload

---
+++ Task1A/DisCatStateGen.py
@@ -0,0 +1,32 @@
+"DisCatStateGen"
+""""Dissipative Cat State from a Two-Photon Exchange Hamiltonian Generation"""
+
+import dynamiqs as dq
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+import numpy as np
+
+# simulation parameters
+n = 5         # Hilbert space size
+g= 1
+epsilon = -4
+kappa = 10
+ntsave = 201   # number of saved states
+T = 4
+
+# operators
+a = dq.tensor(dq.destroy(n), np.eye(n))
+b = dq.tensor(np.eye(n), dq.destroy(n))
+
+
+H = g * a.dag() @ a.dag() @ b + g * a @ a @ b.dag() + epsilon * (b + b.dag())
+jump_ops = [jnp.sqrt(kappa) * a]
+
+# initial state
+psi0 = dq.tensor(dq.basis(n, 0), (dq.basis(n, 0)))
+
+# save times
+tsave = jnp.linspace(0.0, T, ntsave)
+
+# run simulation
+result = dq.mesolve(H, jump_ops, psi0, tsave)

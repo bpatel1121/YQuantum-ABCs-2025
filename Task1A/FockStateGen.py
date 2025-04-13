6d24784f1beef57964320c92bc4fa0ac27115892
bpatel1121 <157432774+bpatel1121@users.noreply.github.com>
2025-04-13 03:50:18 -0700

    Update FockStateGen.py

--- Task1A/FockStateGen.py
+++ Task1A/FockStateGen.py
@@ -5,6 +5,6 @@ plot_wigner = dq.plot.wigner
 import matplotlib.pyplot as plt  
 
 #Fock Sim
-state = fock(10, 1)
+state = fock(10, 1) #Initialize Fock state with dimension 10 and 1 quantum
 plot_wigner(state)
-plt.show()
+plt.show() 

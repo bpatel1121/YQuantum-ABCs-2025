515f7d1c14b992757223d2831ca833143ff3cd7e
bpatel1121 <157432774+bpatel1121@users.noreply.github.com>
2025-04-13 08:51:27 -0700

    Update listCreation.py

--- Task1C/listCreation.py
+++ Task1C/listCreation.py
@@ -5,20 +5,19 @@ def fidelity(rho_true, rho_est):
     sqrt_intermediate = sc.linalg.sqrtm(intermediate)
     return np.real(np.trace(sqrt_intermediate))**2
 
-rho = density_from_wigner(xvec, yvec, w, alpha0=0, num_points=2000, num_batches=20, radius=1.2, N=10, fock=False)
+rho = density_from_wigner(xvec, yvec, w, alpha0=0, num_points=2000, num_batches=20, radius=1.2, N=10, fock=False) #convert raw data into this density matrix
 
+sigma_list = [0.05,.1,.5,1] #choose the arbitrary sigmas
+rho_with_noise_list = []    #initial list when noise(sigma) is applied
 
-sigma_list = [0.05,.1,.5,1]
-rho_with_noise_list = []
-
-for sigmas in sigma_list:
+for sigmas in sigma_list: #apply gaussian noise to w and create list of new w's with different noise applied(per different sigmas)
     w_noise = add_gaussian_noise(w, sigmas)
 
     rho_with_noise = density_from_wigner(xvec, yvec, w_noise, alpha0=0, num_points=2000, num_batches=20, radius=1.2, N=10, fock=False)
-    rho_with_noise_list.append(rho_with_noise)
+    rho_with_noise_list.append(rho_with_noise) #create the rho with noise lists
 
 fidelities = [] 
-for rho_reconstructed in rho_with_noise_list:
+for rho_reconstructed in rho_with_noise_list: #create the list of fidelity values, for each w with noise applied
     f = fidelity(rho, rho_reconstructed)
     fidelities.append(f)
 

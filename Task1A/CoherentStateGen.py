4c8a375640367a704c2b121c3caaa4c98773c1c6
bpatel1121 <157432774+bpatel1121@users.noreply.github.com>
2025-04-13 03:54:48 -0700

    Update CoherentStateGen.py

--- Task1A/CoherentStateGen.py
+++ Task1A/CoherentStateGen.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 
 #Coherent Sim
-alpha = 2.0
-state = coherent(10, alpha)
+alpha = 2.0 #set alpha  
+state = coherent(10, alpha) #coherent state with 10 dimensions and alpha value
 plot_wigner(state)
 plt.show()

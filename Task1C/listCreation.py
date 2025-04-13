bf78de7b7d87a2d1f3e429ae69c8b73e7c2c247e
bpatel1121 <157432774+bpatel1121@users.noreply.github.com>
2025-04-13 08:45:14 -0700

    Create listCreation.py

---
+++ Task1C/listCreation.py
@@ -0,0 +1,24 @@
+def fidelity(rho_true, rho_est):
+    """Compute the fidelity between two density matrices, rho_true and rho_est."""
+    sqrt_rho = sc.linalg.sqrtm(rho_true)
+    intermediate = sqrt_rho @ rho_est @ sqrt_rho
+    sqrt_intermediate = sc.linalg.sqrtm(intermediate)
+    return np.real(np.trace(sqrt_intermediate))**2
+
+rho = density_from_wigner(xvec, yvec, w, alpha0=0, num_points=2000, num_batches=20, radius=1.2, N=10, fock=False)
+
+
+sigma_list = [0.05,.1,.5,1]
+rho_with_noise_list = []
+
+for sigmas in sigma_list:
+    w_noise = add_gaussian_noise(w, sigmas)
+
+    rho_with_noise = density_from_wigner(xvec, yvec, w_noise, alpha0=0, num_points=2000, num_batches=20, radius=1.2, N=10, fock=False)
+    rho_with_noise_list.append(rho_with_noise)
+
+fidelities = [] 
+for rho_reconstructed in rho_with_noise_list:
+    f = fidelity(rho, rho_reconstructed)
+    fidelities.append(f)
+

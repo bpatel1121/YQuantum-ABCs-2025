84c5c41277cfd5095229c7392bd1f5b1836b5130
bpatel1121 <157432774+bpatel1121@users.noreply.github.com>
2025-04-13 01:24:54 -0700

    Delete TASK1C directory

--- TASK1C/addGausNoise.py
+++
@@ -1,9 +0,0 @@
-"AddGausNosie"
-import numpy as np
-
-def add_gaussian_noise(W, sigma):
-    """
-    Adds Gaussian noise with standard deviation sigma to the given Wigner data.
-    """
-    noise = np.random.normal(loc=0, scale=sigma, size=W.shape)
-    return W + noise
